1ff4d39b2d08edd04979e1e47356505668e1a344
Chirag <chirag@tokentraxx.com>
2025-03-02 00:15:01 +0530

    check

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ loadEnvConfig(projectDir);
 
 console.log('publixkey', process.env.PUBLIC_KEY === "checking2")
 console.log('publixkey', process.env.PRIVATE_KEY === "checking")
-console.log('publixkey', process.env.NEXT_CHECK === "WORKING")
+console.log('publixkey', process.env.NEXT_CHECK == "WORKING")
 console.log('publixkey', process.env.NEXT_CHECK === "checking")
