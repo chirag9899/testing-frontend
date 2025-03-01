2ccd17217c10cb06af7d62579b2f163653adcdf2
Chirag <chirag@tokentraxx.com>
2025-03-02 00:23:28 +0530

    check

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
 
-console.log('publixkey', process.env.PUBLIC_KEY === "checking2")
-console.log('publixkey', process.env.PRIVATE_KEY === "checking")
+console.log('publixkey', process.env.PUBLIC_KEY == "checking2")
+console.log('publixkey', process.env.PRIVATE_KEY == "checking")
 console.log('publixkey', process.env.NEXT_CHECK == "WORKING")
-console.log('publixkey', process.env.NEXT_CHECK === "checking")
-console.log('LAST_RIDE', process.env.NEXT_CHECK === "WORKING NEXT_CHECK2=YES_WORKING")
-console.log('LAST_RIDE', process.env.NEXT_CHECK === undefined)
+console.log('publixkey', process.env.NEXT_CHECK == "checking")
+console.log('LAST_RIDE', process.env.NEXT_CHECK == "WORKING NEXT_CHECK2=YES_WORKING")
+console.log('LAST_RIDE', process.env.NEXT_CHECK == undefined)
